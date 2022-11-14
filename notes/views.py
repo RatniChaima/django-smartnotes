03dda82df7de64e8d6a4080162bfe064def2c94d
RatniChaima <ratnichayma@gmail.com>
2022-11-14 22:45:28 +0100

    Create Apps

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Notes
+
+#we are quering for all notes and send them to the template
+# this way when the template is rendered all the information coming directly
+# from the database will be available
+def list(request):
+    all_notes = Notes.objects.all()
+    return render(request, 'notes/notes_list.html',{'notes': all_notes})
