71a3d075ca0514979dc9ef82fcab1e0f6ee21d02
RatniChaima <ratnichayma@gmail.com>
2022-11-15 22:23:14 +0100

    class-based views

--- notes/views.py
+++ notes/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 from .models import Notes
 
 #we are quering for all notes and send them to the template
 # this way when the template is rendered all the information coming directly
 # from the database will be available
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html',{'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = "note"
+
 
 def detail(request, pk):
     try:
