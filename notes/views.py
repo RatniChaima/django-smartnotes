1ebbb6dd0e184f12ad0e379b452ffefcaa3be66d
RatniChaima <ratnichayma@gmail.com>
2022-11-16 21:38:30 +0100

    CRUD

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,10 @@
 import imp
 from django.shortcuts import render
 from django.http import Http404
+from django.http.response import HttpResponseRedirect
 from django.views.generic import UpdateView, CreateView, DetailView, ListView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
@@ -11,24 +13,37 @@ class NotesDeleteView(DeleteView):
     model = Notes
     success_url = '/smart/notes'
     template_name = 'notes/notes_delete.html'
+    login_url = "/admin"
 
 class NotesUpdateView(UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/admin"
 
 class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = "/admin"
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 #we are quering for all notes and send them to the template
 # this way when the template is rendered all the information coming directly
 # from the database will be available
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = "/admin" #if not logged in > they will be redirected to the /admin
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
@@ -41,3 +56,4 @@ def detail(request, pk):
     except Notes.DoesNotExist:
         raise Http404("Note doesn't exist")
     return render(request, 'notes/notes_detail.html',{'note': note})
+
